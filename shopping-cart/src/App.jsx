4fb09b223c2b18376ed9bcb7c04dc2514d152905
eliashz <eliashz@gmail.com>
2023-02-28 18:19:54 +0100

    maxPrice

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Products from "./components/Products";
 import { useFetch } from "./hooks/";
 import { url } from "./constants";
 import { products as initialProducts } from "../db.json";
 
 function App() {
-  // const { data, loading, isError, isSuccess } = useFetch(url.products);
+  const { data, loading, isError, isSuccess } = useFetch(url.products);
+  const [products, setProducts] = useState([]);
+  const [max, setMax] = useState(0);
 
-  const [products] = useState(initialProducts);
-
+  useEffect(() => {
+    if (!data) return;
+    setProducts(data);
+    setMax(Math.max(...data.map((d) => d.price)));
+    console.log(max);
+  }, [data]);
   const [filters, setFilters] = useState({
     category: "all",
-    minPrice: 0,
+    maxPrice: max,
   });
+
+  console.log(filters);
 
   const filterProducts = (products) => {
     return products.filter((product) => {
       return (
-        product.price >= filters.minPrice &&
+        product.price <= filters.maxPrice &&
         (filters.category === "all" || product.category === filters.category)
       );
     });
   };
 
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
-      {/*       {loading && <h5>Loading...</h5>}
+      {loading && <h5>Loading...</h5>}
       {isError && <h5>Error</h5>}
-      {isSuccess && <Products products={products} />} */}
-      <Products products={products} />
+      {isSuccess && <Products products={filteredProducts} />}
+      {/* <Products products={products} /> */}
     </>
   );
 }
