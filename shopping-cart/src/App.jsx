be2c53d4573d907c05877bb89e7afaa3fdda79da
eliashz <eliashz@gmail.com>
2023-02-28 17:46:35 +0100

    back to initial

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Products from "./components/Products";
 import { useFetch } from "./hooks/";
 import { url } from "./constants";
+import { products as initialProducts } from "../db.json";
 
 function App() {
-  const { data, loading, isError, isSuccess } = useFetch(url.products);
+  // const { data, loading, isError, isSuccess } = useFetch(url.products);
+
+  const [products] = useState(initialProducts);
+
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
 
   return (
     <>
-      {loading && <h5>Loading...</h5>}
+      {/*       {loading && <h5>Loading...</h5>}
       {isError && <h5>Error</h5>}
-      {isSuccess && <Products products={data} />}
+      {isSuccess && <Products products={products} />} */}
+      <Products products={products} />
     </>
   );
 }
