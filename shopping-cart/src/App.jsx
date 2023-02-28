0e846d61c625895beaa667aee4d5c1a256b4807d
eliashz <eliashz@gmail.com>
2023-02-28 11:30:56 +0100

    wip fetch

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -2,6 +2,7 @@ import { products } from "./mocks/products.json";
 import Products from "./components/Products";
 
 function App() {
+  url
   return <Products products={products} />;
 }
 
---
+++ shopping-cart/src/constants/url.js
@@ -0,0 +1,1 @@
+export default { products: "http://localhost:3001/products" };
