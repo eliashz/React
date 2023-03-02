c4ef88b7272030d87f6a49e02c1a653a20e429c9
eliashz <eliashz@gmail.com>
2023-03-02 19:14:59 +0100

    context

--- shopping-cart/src/context/filters.js
+++ shopping-cart/src/context/filters.js
@@ -2,3 +2,13 @@ import { createContext } from "react";
 
 export const FiltersContext = createContext();
 
+export function FiltersProvider ({ children }) {
+
+    return (
+        <FiltersContext.Provider value={{ 
+            category: 'all',
+            maxPrice: 1000
+        }}>
+            {children}
+        </FiltersContext.Provider >
+    }
