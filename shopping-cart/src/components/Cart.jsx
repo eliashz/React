68ee1d26feb3ce63872152602fbba708fc671bfc
eliashz <eliashz@gmail.com>
2023-03-03 19:25:07 +0100

    Cart wip

--- shopping-cart/src/components/Cart.jsx
+++ shopping-cart/src/components/Cart.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useId } from "react";
+import { CartIcon, ClearCartIcon, RemoveFromCartIcon } from "./Icons";
 
 const Cart = () => {
+  const cartCheckboxId = useId();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <label className="cart-button" htmlFor={cartCheckboxId}>
+        <CartIcon />
+      </label>
+      <input id={cartCheckboxId} type="checkbox" hidden />
+      <aside className="cart">
+        <ul>
+          <li>
+            <img src="" alt="" />
+          </li>
+          <div>
+            <strong>iPhone</strong> - $1490
+          </div>
+        </ul>
+      </aside>
+    </>
+  );
+};
 
-export default Cart
+export default Cart;
